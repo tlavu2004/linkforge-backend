fb637a45f6dd6de081e079a909abc0fcf113b347
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-02-23 22:11:35 +0700

    feat(tests): disable filters in MockMvc for RedirectControllerTest and ShortLinkControllerTest

--- src/test/java/com/tlavu/linkforge/presentation/controller/RedirectControllerTest.java
+++ src/test/java/com/tlavu/linkforge/presentation/controller/RedirectControllerTest.java
@@ -17,7 +17,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 @WebMvcTest(RedirectController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class RedirectControllerTest {
 
         @Autowired
