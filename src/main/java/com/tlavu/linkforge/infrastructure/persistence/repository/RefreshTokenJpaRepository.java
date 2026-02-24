c2190f1949d7636bbc7220bfaff7ec8bbf09f663
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-02-25 00:07:18 +0700

    feat(auth): add transactional annotations to delete methods in RefreshTokenJpaRepository

--- src/main/java/com/tlavu/linkforge/infrastructure/persistence/repository/RefreshTokenJpaRepository.java
+++ src/main/java/com/tlavu/linkforge/infrastructure/persistence/repository/RefreshTokenJpaRepository.java
@@ -2,7 +2,9 @@ package com.tlavu.linkforge.infrastructure.persistence.repository;
 
 import com.tlavu.linkforge.infrastructure.persistence.entity.RefreshTokenJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,7 +12,11 @@ import java.util.Optional;
 public interface RefreshTokenJpaRepository extends JpaRepository<RefreshTokenJpaEntity, Long> {
     Optional<RefreshTokenJpaEntity> findByToken(String token);
 
+    @Modifying
+    @Transactional
     void deleteByToken(String token);
 
+    @Modifying
+    @Transactional
     void deleteByUserId(Long userId);
 }
