5d8f4a22ffd8330783f11975e225c15d68fd5c4a
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-02-27 00:36:46 +0700

    feat(exception): add AdTokenVerificationException to bad request handler

--- src/main/java/com/tlavu/linkforge/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tlavu/linkforge/presentation/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.tlavu.linkforge.domain.exception.InvalidShortLinkException;
 import com.tlavu.linkforge.domain.exception.InvalidUrlException;
 import com.tlavu.linkforge.domain.exception.ShortLinkExpiredException;
 import com.tlavu.linkforge.domain.exception.ShortLinkNotFoundException;
+import com.tlavu.linkforge.domain.exception.AdTokenVerificationException;
 import com.tlavu.linkforge.presentation.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +40,8 @@ public class GlobalExceptionHandler {
             InvalidUrlException.class,
             InvalidShortCodeException.class,
             InvalidShortLinkException.class,
-            InvalidDeleteTokenException.class
+            InvalidDeleteTokenException.class,
+            AdTokenVerificationException.class
     })
     public ResponseEntity<ApiResponse<Void>> handleBadRequest(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
