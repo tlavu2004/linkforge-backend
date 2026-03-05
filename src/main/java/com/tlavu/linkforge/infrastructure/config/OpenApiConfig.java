0f1f9199c8a29c01c25c2c1c3a133bdf18cfe6f4
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-03-05 11:42:45 +0700

    fix(OpenApiConfig): update support email in API documentation

--- src/main/java/com/tlavu/linkforge/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/tlavu/linkforge/infrastructure/config/OpenApiConfig.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "LinkForge API", version = "1.0", description = "REST API Documentation for the LinkForge URL Shortener Project", contact = @Contact(name = "Support", email = "support@linkforge.app"), license = @License(name = "MIT License", url = "https://opensource.org/licenses/MIT")), servers = {
-        @Server(description = "Local DEV Environment", url = "http://localhost:8080")
+@OpenAPIDefinition(info = @Info(title = "LinkForge API", version = "1.0", description = "REST API Documentation for the LinkForge URL Shortener Project", contact = @Contact(name = "Support", email = "truongleanhvu20052004@gmail.com"), license = @License(name = "MIT License", url = "https://opensource.org/licenses/MIT")), servers = {
+                @Server(description = "Local DEV Environment", url = "http://localhost:8080")
 })
 public class OpenApiConfig {
 }
